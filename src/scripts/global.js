4bdea1e7582c66b290a0a71bac11cf899b6a1d7d
Jc Contreras Meneses <Jc Contreras Meneses>
2017-03-12 22:02:43 -0500

    Desarrollo de proyecto concluido

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -2,21 +2,22 @@
   constants and global functions
 */
 
-var JSON_FILE = '/books_schema.json';
+var JSON_FILE = '/books-schema.json';
 
 /*
  @method loadJSON
  source: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
 */
-var loadJSON = function(url, callback){
+
+function loadJSON(callback) {
+
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open("GET", url, true);
-    xobj.onreadystatechange = function(responseText){
-        if(xobj.readyState == 4 && xobj.status == "200"){
-            var content = JSON.parse(xobj.responseText);
-            callback.call(this, content);
-        }
+    xobj.open('GET', JSON_FILE , true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
     };
     xobj.send(null);
-};
+ }
--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,3 +1,81 @@
-function searchForm(){
-    // code here
+
+/* ========================================================
+    Valida el estado del boton de busqueda
+   ======================================================== */
+
+function btnValidate(){
+
+  if(document.getElementById("text-search").value == ""){
+    document.getElementById("btn-submit").disabled = true;
+  }else{
+    document.getElementById("btn-submit").disabled = false;
+  }
+
+}
+
+/* ========================================================
+    Carga los datos de las busquedas por defecto
+   ======================================================== */
+
+function loadData() {
+ loadJSON(function(response) {
+
+    var objJson = JSON.parse(response);
+
+    var x = 0;
+
+    while(objJson.entities.saved[x] != undefined){
+      var savedLabel = objJson.entities.saved[x].label;
+      document.getElementById('listSearchSaved')
+        .innerHTML += "<li id='link" + x + "'>" +
+                        "<a href='' id='searchSaved" + x +"' onMouseOver='showOptions(this.id)' onMouseOut='hideOptions(this.id)'>" + savedLabel +"</a><br>" +
+                        "<div id='options" + x + "' class='options-search' onMouseOver='showOptions(this.id)' onMouseOut='hideOptions(this.id)'></div>" +
+                      "</li>";
+      x++;
+    }
+
+ });
+}
+
+/* ========================================================
+    Muestra las opciones de las busquedas
+   ======================================================== */
+
+function showOptions(id){
+
+  var sizeId = id.length;
+  var identifier = id.substring(0 , sizeId - 1);
+
+  if(identifier == "searchSaved"){
+    var numberLink = id.substring(sizeId - 1);
+    var link = document.getElementById("options" + numberLink);
+    link.innerHTML = "<a href=''>Editar</a><span> | </span><a href='javascript:void(0);' id='deleteSearch" + numberLink + "' onClick='hideOptions(this.id)'>Eliminar</a>";
+    link.style.display = "block";
+  }
+
+  if(identifier == "options"){
+    var numberLink = id.substring(sizeId - 1);
+    var link = document.getElementById("options" + numberLink);
+    link.style.display = "block";
+  }
+
+}
+
+/* ========================================================
+    Oculta las opciones de las busquedas
+   ======================================================== */
+
+function hideOptions(id){
+
+  var sizeId = id.length;
+  var identifier = id.substring(0 , sizeId - 1);
+  var numberLink = id.substring(sizeId - 1);
+  var link = document.getElementById("options" + numberLink);
+  link.style.display = "none";
+
+  if(identifier == "deleteSearch"){
+    var link = document.getElementById("link" + numberLink);
+    link.style.display = "none";
+  }
+
 }
